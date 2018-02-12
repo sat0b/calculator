ef32d0bfbe7ba372d5890b784a71972eafce6c67
sat0b <sat0b.dev@gmail.com>
2018-02-12 23:20:32 +0900

    Support of file execution

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cctype>
+#include <fstream>
 #include <iostream>
 #include <istream>
 #include <map>
@@ -325,10 +326,8 @@ public:
   }
 };
 
-int main(int argc, char **argv) {
+void repl() {
   Parser parser;
-
-  // iterpreter mode
   std::string line;
   std::cout << ">> ";
   while (getline(std::cin, line)) {
@@ -337,5 +336,30 @@ int main(int argc, char **argv) {
       line += ";";
     parser.run(line);
     std::cout << ">> ";
+  }
+}
+
+void run(std::string fileName) {
+  std::ifstream ifs(fileName);
+  if (!ifs) {
+    std::cerr << "Cannot open " << fileName << std::endl;
+    return;
+  }
+  Parser parser;
+  std::string line;
+  while (getline(ifs, line)) {
+    int n = line.length() - 1;
+    if (line[n] != '\n' && line[n] != ';')
+      line += ";";
+    parser.run(line);
+  }
+}
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    std::string fileName = std::string(argv[1]);
+    run(fileName);
   }
 }
