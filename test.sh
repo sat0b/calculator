5c5249ef1e1ff82f49e201cfe88ee450773ae1e0
Junichi Sato <sat0b.dev@gmail.com>
2018-03-15 23:42:11 +0900

    Add test case

--- test.sh
+++ test.sh
@@ -19,11 +19,11 @@ assert-equal test/le.ca 1
 assert-equal test/mod.ca 2
 assert-equal test/name_error.ca "Name error, no such a variable a"
 assert-equal test/syntax_error.ca "Syntax error, expected =, but got =="
+assert-equal test/loop.ca 55
+assert-equal test/nested_for.ca 150
 # assert-equal test/if_true.ca 10
 # assert-equal test/if_false.ca 5
 # assert-equal test/if_elseif_else.ca 2
-# assert-equal test/loop.ca 55
-# assert-equal test/nested_for.ca 150
 # assert-equal test/func.ca 25
 # assert-equal test/func_var_arg.ca 15
 # assert-equal test/hello.ca "Hello, world!"
